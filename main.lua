c3e9509c3be7321a09954c1f72a10d90be535a9c
Kevin Mach <mr.kevinmach@gmail.com>
2017-11-02 21:08:54 -0700

    Added UI and  fixes for other Methods

--- main.lua
+++ main.lua
@@ -19,6 +19,11 @@ function timelineBattle (times)
     if exists("close.png") then
         click("close.png")
     end
+    wait(2)
+    if exists("recommended.png") then
+        click("close.png")
+    end
+
     click("search_for.png")
 
     -- notice you cannot hold more than 60000 honor tokens
@@ -60,31 +65,91 @@ function chooseChapter (chapterNumber)
     until exists("chapter" .. chapterNumber .. ".png")
 end 
 
-function chooseStage (stageNumber)
-    Settings:set("MinSimilarity", 0.85)
-    click("stage" .. stageNumber .. ".png")
+function chooseStage (chapterNumber, stageNumber)
+    ch1_offset = 115 
+    ch2_offset = 116
+    ch3_offset = 115 
+    ch4_offset = 115 
+    ch5_offset = 115 
+    ch6_offset = 115 
+    ch7_offset = 115 
+    ch8_offset = 115 
+    ch9_offset = 115 
+    ch10_offset = 115 
+    ch11_offset = 115 
+    ch12_offset = 115 
+
+    -- this will be the offset value used for all logic 
+    offset = 0 
+    if chapterNumber == 1 then 
+        offset = ch1_offset
+    elseif chapterNumber == 2 then
+        offset = ch2_offset
+    elseif chapterNumber == 2 then
+        offset = ch2_offset
+    elseif chapterNumber == 3 then
+        offset = ch3_offset
+    elseif chapterNumber == 4 then
+        offset = ch4_offset
+    elseif chapterNumber == 5 then
+        offset = ch5_offset
+    elseif chapterNumber == 6 then
+        offset = ch6_offset
+    elseif chapterNumber == 7 then
+        offset = ch7_offset
+    elseif chapterNumber == 8 then
+        offset = ch8_offset
+    elseif chapterNumber == 9 then
+        offset = ch9_offset
+    elseif chapterNumber == 10 then
+        offset = ch10_offset
+    elseif chapterNumber == 11 then
+        offset = ch11_offset
+    elseif chapterNumber == 12 then
+        offset = ch12_offset
+    end 
+    chapter_pattern = Pattern("ch" .. chapterNumber .. "_offset.png")
+    stage_offset = click(chapter_pattern:targetOffset(offset*(stageNumber - 1),0)) --if stage 2 is choosen by user click based on offset value
+
 end 
 
 function runStoryMissions(chapter, stage, times)
 
+    isRunning = false
+
     waitClick("enter.png")
     waitClick("story.png")
     chooseChapter(chapter)
-    chooseStage(stage)
-    waitClick("start.png")
-    -- loop is configured for number of times 3 times = 3-2 +1 
-    for i= times-1,1,-1
+    chooseStage(chapter,stage)
+    
+    for i = times,1, -1
     do
+        waitClick("start.png") 
+        isRunning = true 
+        while (isRunning) do
+            if exists("repeat.png") then
+                isRunning = false
+            end
+        end
+        
         if exists("herorankup.png") then
             waitClick("cancel.png")
         end
-        waitClick("repeatstory.png",200)
-        if exists("jocasta.png", 10) then 
-            click("ok.png")
-        end 
-        waitClick("start.png",30)
+
+        waitClick("repeat.png")
+        wait(3)
+
+        if exists("shield_safe.png") then
+                click("ok.png")
+        end
+        wait(3)
+
     end 
-    waitClick("leave.png",200)
+
+    wait(3)
+    waitClick("home.png")
+
+    print("Ran Normal Missions: " .. times .. " times!")
 
 end 
 
@@ -96,7 +161,7 @@ function getCoordinates(pattern)
         y1 = m:getY()
 
         print("x:" .. x1 .. "y:" .. y1)
-    end
+    end 
 end 
 
 function runDailyMission()
@@ -106,21 +171,27 @@ function runDailyMission()
 end
 
 --FINISHED WORKS GOOD!
-function runSpecialMission(times)
+function runSpecialMission(times, hiddenTicket)
     isRunning = false
 
     waitClick("enter.png")
     waitClick("special.png")
-    waitClick("new_avengers.png")
-    waitClick("new_york.png")
+    waitClick("sandman.png")
     wait(1)
-    swipe("new_york4.png", "new_york1.png")
     waitClick("enter_level.png")
 
     for i = times,1, -1
     do
         wait(4)
         waitClick("start.png")
+        wait(3)
+
+        if exists("notice.png") then
+            if hiddenTicket == true then
+
+            end -- if want hidden ticket click ok else click cancel
+            click("ok.png")
+        end
         isRunning= true
 
         while (isRunning) do
@@ -253,27 +324,33 @@ function findAllImp(pattern)
 
     -- print (table.getn(enter)) -- print the number of matches
 end
--- Content Status Board
 
---Dark Dimension
--- Dark Dimension
-
--- Daily Challenge
--- Clear 1 Story Stage
--- Clear 1 Daily Mission Stage
--- Send 1 Alliance Point
--- Clear 1 Special Mission
--- Play Co-Op Once
--- Claim 25 Crystals
-
---findAllImp("enter_level.png")
 -- runDarkDimension(2)
--- timelineBattle(5)
- --chooseChapter(12)
-  --chooseStage(1)
---runStoryMissions(3,6,2)
+--timelineBattle(9)
+--chooseChapter(12)
+--chooseStage(1,8)
+-- runStoryMissions(4,5,1)
 -- getCoordinates()
 -- local l=Location(123,411)
--- click(l)
-runSpecialMission(15)
+-- runSpecialMission(20)
 -- runMemoryMission(2)
+
+-- Start Program and create UI
+function UI() 
+    dialogInit()
+    addTextView("Run Story Missions(chapter, stage, times):")
+    addEditText("story_chapter", "1")
+    addEditText("story_stage", "1")
+    addEditText("story_times", "1")
+    newRow()
+
+    addTextView("Run Memory Mission:")
+    addEditText("memory_times", "1")
+
+    dialogShow("Future Fight Bot")
+    runStoryMissions(tonumber(story_chapter),tonumber(story_stage),tonumber(story_times))
+    wait(3)
+    runMemoryMission(tonumber(memory_times))
+end
+
+UI()
